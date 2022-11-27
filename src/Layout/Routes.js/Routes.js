3fe464b981578a7f9474cb3185363ac18a59a49a
Nakib <nakkibcse@gmail.com>
2022-11-28 00:43:00 +0600

    add seller route

--- src/Layout/Routes.js/Routes.js
+++ src/Layout/Routes.js/Routes.js
@@ -4,11 +4,23 @@ import Main from "./Main";
 import Login from "./../Login/Login";
 import Register from "./../Register/Register";
 import SingleCategories from "../Home/Categories/SingleCategories";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
+import DashboardLayout from "../Dashboard/DashboardLayout/DashboardLayout";
+import AdminRoute from "../AdminRoute/AdminRoute";
+import AddProduct from "../AddProduct/AddProduct";
+import Dashboard from "../Dashboard/Dashboard";
+import MyProduct from "../Dashboard/MyProduct/MyProduct";
+import AllBuyer from "../Dashboard/AllBuyer/AllBuyer";
+import AllSeller from "../Dashboard/AllSeller/AllSeller";
+import Blog from "./../Home/Blog/Blog";
+import Notfound from "../Notfound/Notfound";
+import Report from "../Dashboard/Report/Report";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    errorElement: <Notfound></Notfound>,
     children: [
       {
         path: "/",
@@ -17,6 +29,10 @@ export const router = createBrowserRouter([
       {
         path: "/home",
         element: <Home />,
+      },
+      {
+        path: "/blog",
+        element: <Blog />,
       },
       {
         path: "/login",
@@ -29,9 +45,61 @@ export const router = createBrowserRouter([
       {
         path: "/category/:userId",
         loader: async ({ params }) => {
-          return fetch(`http://localhost:5000/category/${params.userId}`);
+          return fetch(
+            `https://assignment-12-server-orcin.vercel.app/category/${params.userId}`
+          );
         },
-        element: <SingleCategories />,
+        element: (
+          <PrivateRoute>
+            <SingleCategories />
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <PrivateRoute>
+        <DashboardLayout></DashboardLayout>
+      </PrivateRoute>
+    ),
+    children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard></Dashboard>,
+      },
+      {
+        path: "/dashboard/addProduct",
+        element: <AddProduct></AddProduct>,
+      },
+      {
+        path: "/dashboard/myProduct",
+        element: <MyProduct></MyProduct>,
+      },
+      {
+        path: "/dashboard/allBuyer",
+        element: (
+          <AdminRoute>
+            <AllBuyer></AllBuyer>
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "/dashboard/allSeller",
+        element: (
+          <AdminRoute>
+            <AllSeller></AllSeller>
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "/dashboard/report",
+        element: (
+          <AdminRoute>
+            <Report></Report>
+          </AdminRoute>
+        ),
       },
     ],
   },
